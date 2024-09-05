b8c59161ad13164d20c959f98abe925a93a14751
Jez <jezdavisuk@gmail.com>
2024-09-05 10:26:45 +0100

    Initial commit

---
+++ src/main/java/com/example/Record_Shop/RecordShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Record_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Record_Shop/RecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Record_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
