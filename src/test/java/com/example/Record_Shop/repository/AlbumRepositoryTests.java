e985da1b6452805b7203b5a781ac757786aa9a3a
Jez <jezdavisuk@gmail.com>
2024-09-06 11:17:44 +0100

    add: testing repository layer with in-built CrudRepository findById() method to retrieve albums/records by their assigned ID fields

--- src/test/java/com/example/Record_Shop/repository/AlbumRepositoryTests.java
+++ src/test/java/com/example/Record_Shop/repository/AlbumRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,21 +27,27 @@ class AlbumRepositoryTests {
         expectedList.add(new Album(4L, "Cher Lloyd", "Cher Lloyd", 2011, Genre.SOUL, 100, true));
         expectedList.add(new Album(5L, "Meteora", "Linkin Park", 2003, Genre.METAL, 29, true));
 
-
-        Album album1 = new Album(1L, "Total Life Forever", "Foals", 2010, Genre.INDIE, 15, true);
-        albumRepository.save(album1);
-        Album album2 = new Album(2L, "Settle", "Disclosure", 2013, Genre.HOUSE, 21, true);
-        albumRepository.save(album2);
-        Album album3 = new Album(3L, "Curtis", "Curtis Mayfield", 1970, Genre.SOUL, 0, false);
-        albumRepository.save(album3);
-        Album album4 = new Album(4L, "Cher Lloyd", "Cher Lloyd", 2011, Genre.SOUL, 100, true);
-        albumRepository.save(album4);
-        Album album5 = new Album(5L, "Meteora", "Linkin Park", 2003, Genre.METAL, 29, true);
-        albumRepository.save(album5);
+        albumRepository.saveAll(expectedList);
 
         Iterable<Album> albums = albumRepository.findAll();
 
         assertThat(albums).hasSize(5);
         assertThat(expectedList).isEqualTo(albums);
+    }
+
+    @Test
+    public void testGetAlbumByID() {
+
+        List<Album> expectedList = new ArrayList<>();
+        expectedList.add(new Album(1L, "Total Life Forever", "Foals", 2010, Genre.INDIE, 15, true));
+        expectedList.add(new Album(2L, "Settle", "Disclosure", 2013, Genre.HOUSE, 21, true));
+        expectedList.add(new Album(3L, "Curtis", "Curtis Mayfield", 1970, Genre.SOUL, 0, false));
+        expectedList.add(new Album(4L, "Cher Lloyd", "Cher Lloyd", 2011, Genre.SOUL, 100, true));
+        expectedList.add(new Album(5L, "Meteora", "Linkin Park", 2003, Genre.METAL, 29, true));
+
+        albumRepository.saveAll(expectedList);
+        
+        assertThat(albumRepository.findById(3L)).isEqualTo(Optional.of(expectedList.get(2)));
+        assertThat(albumRepository.findById(5L)).isEqualTo(Optional.of(expectedList.get(4)));
     }
 }
